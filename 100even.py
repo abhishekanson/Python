bc31ce354f9f23b2c19f83f40d530ad9ec443697
abhishekanson <abhishekanson04@gmail.com>
2025-09-24 11:39:15 +0530

    athe narakam

---
+++ 100even.py
@@ -0,0 +1,7 @@
+print("Sum of first 100 even numbers: ")
+n=2
+sum=0
+for i in range(100):
+    sum+=n
+    n=n+2
+print(sum)
---
+++ nmultiples.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+n = int(input("Enter no. of multiples: "))
+
+for i in range(1,n+1):
+    print(num*i)
