c7e21f8b2a2616f8a47d06486ae4fbd91b9d516d
abhishekanson <abhishekanson04@gmail.com>
2025-10-22 11:56:01 +0530

    changes

--- graphics.py
+++ graphics.py
@@ -1,6 +1,8 @@
 import turtle
+import matplotlib.pyplot as plt
 
 t = turtle.Turtle()
+
 t.forward(100)
 t.left(90)
 t.forward(100)
@@ -9,3 +11,14 @@ t.forward(100)
 t.left(90)
 t.home()
 turtle.done()
+
+
+x = [0, 1, 2, 3, 4]
+y = [0, 1, 4, 9, 16]
+
+plt.plot(x, y)
+plt.title("Simple Line Plot")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
